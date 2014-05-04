087517f86dbbf9695476eb22fad48f9248c4527d
Japheth <japhethcrawford@gmail.com>
2014-05-04 16:27:55 -0400

    fix test

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,16 @@
 
 (function (){
 	
-// Instantiate the Choreo, using a previously instantiated TembooSession object, eg:
-// TembooSession session = new TembooSession("ACCOUNT_NAME", "APP_NAME", "APP_KEY");
-Tweets tweetsChoreo = new Tweets(session);
+// You'll need a single TembooSession object in your code, eg:
+var tsession = require("temboo/core/temboosession");
+var session = new tsession.TembooSession("appTHEList", "#OnMyBucketList Stream", "aNPqVfiKcbXgzM0dddPti6Nii");
 
-// Get an InputSet object for the choreo
-TweetsInputSet tweetsInputs = tweetsChoreo.newInputSet();
+var Twitter = require("temboo/Library/Twitter/Search");
+
+var tweetsChoreo = new Twitter.Tweets(session);
+
+// Instantiate and populate the input set for the choreo
+var tweetsInputs = tweetsChoreo.newInputSet();
 
 // Set inputs
 tweetsInputs.set_AccessToken("2467151592-AN5SfkF8ToGuAi5x6HUnoOzXx1GnsRUOvNGeu9N");
@@ -16,9 +20,12 @@ tweetsInputs.set_AccessTokenSecret("syTIxpg3VZIteBwPz8YlLQlWqwuO5Zv5UBh3guMv4JMl
 tweetsInputs.set_ConsumerSecret("iaZ438c9Via2nCrueHtjAXo5XHItY91QhkDELDXDCIOGaOPCCZ");
 tweetsInputs.set_ConsumerKey("aNPqVfiKcbXgzM0dddPti6Nii");
 
-// Execute Choreo
-TweetsResultSet tweetsResults = tweetsChoreo.execute(tweetsInputs);
-	
+// Run the choreo, specifying success and error callback handlers
+tweetsChoreo.execute(
+    tweetsInputs,
+    function(results){console.log(results.get_Response());},
+    function(error){console.log(error.type); console.log(error.message);}
+);
 	
 })();
 
